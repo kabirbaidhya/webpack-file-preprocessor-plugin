ebe976e5fc42fb2e65e3290dbb5ed03a3b58d390
rogelio-o <yo@rogelioorts.com>
2019-10-25 21:45:58 +0200

    Add simple example

---
+++ examples/html-minifier/webpack.config.js
@@ -0,0 +1,32 @@
+const webpack = require("webpack");
+const WebpackFilePreprocessorPlugin = require("webpack-file-preprocessor-plugin");
+const minifyHtml = require("html-minifier").minify;
+const path = require("path");
+
+module.exports = {
+  entry: ["./src/index.html"],
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            name: "[name].html"
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new WebpackFilePreprocessorPlugin({
+      // Prints processed assets if set to true (default: false)
+      debug: true,
+      // RegExp pattern to filter assets for pre-processing.
+      pattern: /\.html$/,
+      // Do your processing in this process function.
+      process: source =>
+        minifyHtml(source.toString(), { collapseWhitespace: true })
+    })
+  ]
+};
