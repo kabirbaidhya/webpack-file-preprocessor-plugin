417fef4d64593db7dc2dff67eb43be865af1700d
Anusha S <anushas@thoughtworks.com>
2019-10-27 12:11:32 +0530

    [Issue-13] Add chai and include sample test case for chai <Anusha>

--- test/WebpackFilePreprocessorPlugin.js
+++ test/WebpackFilePreprocessorPlugin.js
@@ -1,9 +1,11 @@
 var assert = require('assert');
+var expect = require('chai').expect;
+
 describe('Basic Mocha String Test', function () {
  it('should return number of charachters in a string', function () {
-        assert.equal("Hello".length, 4);
+        assert.equal("Hello".length, 5);
     });
  it('should return first charachter of the string', function () {
-        assert.equal("Hello".charAt(0), 'H');
+    expect("Hello".charAt(0)).to.eq('H');
     });
 });
