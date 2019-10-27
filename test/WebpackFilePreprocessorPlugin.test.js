87a93603af4ba47efe86a6e0c2c72660a8357bdf
Anusha S <anushas@thoughtworks.com>
2019-10-27 19:34:58 +0530

    [Issue-13] Lint all files <Anusha>

--- test/WebpackFilePreprocessorPlugin.test.js
+++ test/WebpackFilePreprocessorPlugin.test.js
@@ -1,5 +1,5 @@
-describe("sample test case",() => {
-    test('adds 1 + 2 to equal 3', () => {
-        expect(1+2).toBe(3);
-    });
+describe('sample test case', () => {
+  test('adds 1 + 2 to equal 3', () => {
+    expect(1 + 2).toBe(3);
+  });
 });
--- test/util.test.js
+++ test/util.test.js
@@ -1,42 +1,47 @@
 import { validateOptions, forEach } from '../src/util';
 
-describe("util",()=>{
-  describe("forEach",()=>{
-    test("should call the passed callback function with every key-value pair of the object",()=>{
+describe('util', ()=>{
+  describe('forEach', ()=>{
+    test('should call the passed callback function with every key-value pair of the object', ()=>{
       const mockCallBack = jest.fn();
-      const object = { 'id':1, 'name':'dummy'};
-      forEach(object,mockCallBack);
+      const object = { 'id': 1, 'name': 'dummy' };
+
+      forEach(object, mockCallBack);
       expect(mockCallBack).toHaveBeenCalledTimes(2);
       expect(mockCallBack).toHaveBeenNthCalledWith(1, 1, 'id');
       expect(mockCallBack).toHaveBeenNthCalledWith(2, 'dummy', 'name');
     });
   
-    test("should not call callback function if the given object is empty",()=>{
+    test('should not call callback function if the given object is empty', ()=>{
       const mockCallBack = jest.fn();
       const object = {};
-      forEach(object,mockCallBack);
+
+      forEach(object, mockCallBack);
       expect(mockCallBack).toHaveBeenCalledTimes(0);
     });
   });
-  describe("validateOptions",()=>{
-    test("should return true if both the given pattern is a valid Regex and the given process is a function",()=>{
+  describe('validateOptions', ()=>{
+    test('should return true if both the given pattern is a valid Regex and the given process is a function', ()=>{
       const process = () => {};
       const pattern = new RegExp('ab+c');
-      const isValid = validateOptions({pattern, process});
+      const isValid = validateOptions({ pattern, process });
+
       expect(isValid).toEqual(true);
     });
-    test("should return false if the given pattern is a valid Regex and the given process is not a function",()=>{
-      const process = "dummy string";
+    test('should return false if the given pattern is a valid Regex and the given process is not a function', ()=>{
+      const process = 'dummy string';
       const pattern = new RegExp('ab+c');
-      const isValid = validateOptions({pattern, process});
+      const isValid = validateOptions({ pattern, process });
+
       expect(isValid).toEqual(false);
     });
 
-    test("should return false if the given pattern is not a valid Regex but the given process is a function",()=>{
+    test('should return false if the given pattern is not a valid Regex but the given process is a function', ()=>{
       const process = () => {};
-      const pattern = "abcd";
-      const isValid = validateOptions({pattern, process});
+      const pattern = 'abcd';
+      const isValid = validateOptions({ pattern, process });
+
       expect(isValid).toEqual(false);
     });
-  })
+  });
 });
