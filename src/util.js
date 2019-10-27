0794703c44a15db9af2ad51cbc39866931109440
Kabir Baidhya <kabirbaidhya@gmail.com>
2019-10-27 13:30:35 +0545

    Resolve all eslint issues for util.js

--- src/util.js
+++ src/util.js
@@ -1,13 +1,28 @@
+/**
+ * Check if the value is a valid RegExp.
+ *
+ * @param {*} value
+ * @returns {boolean}
+ */
 function isValidRegExp(value) {
   return value instanceof RegExp;
 }
 
+/**
+ * Check if the value is a function.
+ *
+ * @param {*} value
+ * @returns {boolean}
+ */
 function isFunction(value) {
   return typeof value === 'function';
 }
 
 /**
  * Validate plugin options.
+ *
+ * @param {*} options
+ * @returns {boolean}
  */
 export function validateOptions(options) {
   const { pattern, process } = options;
@@ -18,12 +33,12 @@ export function validateOptions(options) {
 /**
  * Loop through each key of an object.
  *
- * @param object
- * @param callback
+ * @param {*} object
+ * @param {Function} callback
  */
 export function forEach(object, callback) {
   for (const key in object) {
-    if (!object.hasOwnProperty(key)) {
+    if (!Object.prototype.hasOwnProperty.call(object, key)) {
       continue;
     }
 
