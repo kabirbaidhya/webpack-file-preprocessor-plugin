eaf3b9891b12ed307ceecad14e2fa05c10f2c425
Anusha S <anushas@thoughtworks.com>
2019-10-28 16:00:30 +0530

    [Issue-17] Fix JSDocs indent <Anusha>

--- examples/js-minifier/src/index.js
+++ examples/js-minifier/src/index.js
@@ -1,10 +1,10 @@
 /**
-   * Add two numbers.
-   * 
-   * @param {number} first The first number.
-   * @param {number} second The second number.
-   * @returns {number} The sum of the two numbers.
-**/
+ * Add two numbers.
+ * 
+ * @param {number} first The first number.
+ * @param {number} second The second number.
+ * @returns {number} The sum of the two numbers.
+ */
 function add(first, second) { 
   return first + second; 
 }
