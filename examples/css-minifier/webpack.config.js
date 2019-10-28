69942f9550cf55855db6c50a7ed993199070d96f
Bhavesh <bhavesh@Administrators-MacBook-Air.local>
2019-10-28 22:06:10 +0530

    css minifier example added

---
+++ examples/css-minifier/webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
+module.exports = {
+  mode: "production",
+  entry: ["./src/style.css"],
+  optimization: {
+    minimizer: [new OptimizeCSSAssetsPlugin({
+      assetNameRegExp: /\.css$/,
+      cssProcessor: require('cssnano'),
+      cssProcessorPluginOptions: {
+        preset: ['default', { discardComments: { removeAll: true } }],
+      },
+      canPrint: true
+    })],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].[hash].css',
+      chunkFilename: '[id].[hash].css',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: (resourcePath, context) => {
+                return path.relative(path.dirname(resourcePath), context) + '/';
+              }
+            }
+          },
+          {
+            loader: 'css-loader',
+            options: {}
+          }
+        ],
+      },
+    ],
+  },
+};
