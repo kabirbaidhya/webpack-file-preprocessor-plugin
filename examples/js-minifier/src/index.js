75a4e38670e8d11070d5546105328ae1a3e2d36e
Anusha S <anushas@thoughtworks.com>
2019-10-28 14:32:48 +0530

    [Issue-17] Fix lint  <Anusha>

--- examples/js-minifier/src/index.js
+++ examples/js-minifier/src/index.js
@@ -1,1 +1,12 @@
-function add(first, second) { return first + second; }
+/**
+   * Add two numbers.
+   * 
+   * @param {number} first The first number.
+   * @param {number} second The second number.
+   * @returns {number} The sum of the two numbers.
+**/
+function add(first, second) { 
+  return first + second; 
+}
+
+add(4, 5);
