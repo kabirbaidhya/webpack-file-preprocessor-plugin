5ca1e0044a5e48d44cb699fb2824a6685c564744
Kabir Baidhya <kabirbaidhya@gmail.com>
2019-10-28 11:00:10 +0545

    Fix Formatting

--- test/util.test.js
+++ test/util.test.js
@@ -1,18 +1,18 @@
 import { validateOptions, forEach } from '../src/util';
 
-describe('util', ()=>{
-  describe('forEach', ()=>{
-    test('should call the passed callback function with every key-value pair of the object', ()=>{
+describe('util', () => {
+  describe('forEach', () => {
+    test('should call the passed callback function with every key-value pair of the object', () => {
       const mockCallBack = jest.fn();
-      const object = { 'id': 1, 'name': 'dummy' };
+      const object = { id: 1, name: 'dummy' };
 
       forEach(object, mockCallBack);
       expect(mockCallBack).toHaveBeenCalledTimes(2);
       expect(mockCallBack).toHaveBeenNthCalledWith(1, 1, 'id');
       expect(mockCallBack).toHaveBeenNthCalledWith(2, 'dummy', 'name');
     });
-  
-    test('should not call callback function if the given object is empty', ()=>{
+
+    test('should not call callback function if the given object is empty', () => {
       const mockCallBack = jest.fn();
       const object = {};
 
@@ -20,15 +20,16 @@ describe('util', ()=>{
       expect(mockCallBack).toHaveBeenCalledTimes(0);
     });
   });
-  describe('validateOptions', ()=>{
-    test('should return true if both the given pattern is a valid Regex and the given process is a function', ()=>{
+
+  describe('validateOptions', () => {
+    test('should return true if both the given pattern is a valid Regex and the given process is a function', () => {
       const process = () => {};
       const pattern = new RegExp('ab+c');
       const isValid = validateOptions({ pattern, process });
 
       expect(isValid).toEqual(true);
     });
-    test('should return false if the given pattern is a valid Regex and the given process is not a function', ()=>{
+    test('should return false if the given pattern is a valid Regex and the given process is not a function', () => {
       const process = 'dummy string';
       const pattern = new RegExp('ab+c');
       const isValid = validateOptions({ pattern, process });
@@ -36,7 +37,7 @@ describe('util', ()=>{
       expect(isValid).toEqual(false);
     });
 
-    test('should return false if the given pattern is not a valid Regex but the given process is a function', ()=>{
+    test('should return false if the given pattern is not a valid Regex but the given process is a function', () => {
       const process = () => {};
       const pattern = 'abcd';
       const isValid = validateOptions({ pattern, process });
